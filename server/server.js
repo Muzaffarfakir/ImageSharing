88b77300c9b7c71f944cc8c371dd8f94111f618c
Muzaffarfakir <98309599+Muzaffarfakir@users.noreply.github.com>
2023-08-15 12:54:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 let express = require("express");
 let app = express();
-let port = 8000;
+let port =  process.env.PORT || 8000;
 let cors = require("cors")
 let bodyparser = require("body-parser");
 let multer = require("multer");
